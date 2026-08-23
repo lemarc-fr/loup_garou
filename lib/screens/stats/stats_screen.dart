f19bb41e784e83327d95cc39163d19fbab8746da
Marc Brunet <marcbrunet92@gmail.com>
2026-08-23 23:26:26 +0200

    null

--- lib/screens/stats/stats_screen.dart
+++ lib/screens/stats/stats_screen.dart
@@ -1,1 +1,10 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class StatsScreen extends StatelessWidget {
+  const StatsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Stats screen');
+  }
+}
